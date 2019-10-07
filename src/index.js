83599b12e41eba7eaf045123f7062e4dfeda46dc
Gregory Kazmierczak <gregory.kazmierczak@lpsolutions.com>
2019-10-07 16:03:27 +0200

    Basic React app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import FormContainer from "./js/components/container/FormContainer.jsx";
 
 class App extends React.Component{
     render(){
         return(
-            <div>Hello World</div>
+            <div>
+                <h1>Hello World</h1>
+                <FormContainer></FormContainer>
+            </div>
         )
     }
 }
